27d8d16edd5d6bfd292976ec096e72099ec93df8
ittanez <a.zenatti@gmail.com>
2025-05-31 18:33:46 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-
-import React from "react";
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+ import React from "react";
+// Commenté temporairement pour résoudre les erreurs de build
+// import { Toaster } from "@/components/ui/toaster";
+// import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -37,8 +37,9 @@ const App = () => {
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <TooltipProvider>
-            <Toaster />
-            <Sonner />
+            {/* Temporairement commenté pour résoudre les erreurs de build */}
+            {/* <Toaster /> */}
+            {/* <Sonner /> */}
             <BrowserRouter>
               <Routes>
                 <Route path="/" element={<Index />} />
